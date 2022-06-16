2ccd3735dd4049f656f5ad27c1f69585fa43a5ab
Mundiaem <machariamundia@gmail.com>
2022-06-16 14:37:24 +0300

    binary search tree for bursary tasks

---
+++ src/BST/Node.java
@@ -0,0 +1,19 @@
+package BST;
+
+/**
+ * created with love by mundiaem
+ * created on 16/06/2022
+ * Time: 13:37
+ * ⚡  - DSA-
+ */
+
+public class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        this.data = item;
+        right = left = null;
+
+    }
+}
