d1276deb5c8828ba364ea76f2fa2d14f2809f8b3
Mundiaem <machariamundia@gmail.com>
2022-11-22 17:20:53 +0300

    String reversal

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -84,6 +84,7 @@ public class ReverseString {
         String ms = "Google";
         StringBuffer stringBufferReverse = new StringBuffer(ms);
         stringBufferReverse.reverse();
+
         System.out.println(stringBufferReverse);
     }
 
@@ -124,5 +125,26 @@ public class ReverseString {
         System.out.println(input);
 
 
+    }
+
+    public String reverseWords(String s) {
+        char[] ca = s.toCharArray();
+        for (int i = 0; i < ca.length; i++) {
+            if (ca[i] != ' ') {   // when i is a non-space
+                int j = i;
+                while (j + 1 < ca.length && ca[j + 1] != ' ') { j++; } // move j to the end of the word
+                reverse(ca, i, j);
+                i = j;
+            }
+        }
+        return new String(ca);
+    }
+
+    private void reverse(char[] ca, int i, int j) {
+        for (; i < j; i++, j--) {
+            char tmp = ca[i];
+            ca[i] = ca[j];
+            ca[j] = tmp;
+        }
     }
 }
