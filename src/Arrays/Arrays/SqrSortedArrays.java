2e065c42dd3469850be204d14bfd251abba85b64
Mundiaem <machariamundia@gmail.com>
2022-12-28 11:02:42 +0300

    Sqr of a sorted array

--- src/Arrays/Arrays/SqrSortedArrays.java
+++ src/Arrays/Arrays/SqrSortedArrays.java
@@ -1,5 +1,7 @@
 package Arrays.Arrays;
 
+import java.util.Arrays;
+
 /**
  * created with  by mundiaem
  * created on 28/12/2022
@@ -8,5 +10,28 @@ package Arrays.Arrays;
  */
 
 public class SqrSortedArrays {
+private static int [] sqrOfSortedArrays(int[] arr){
+    int n= arr.length;
+    int[] results  = new int[n];
+    int i=0, j= n-1;
+    for(int p=n-1; p>=0; p--){
+        if(Math.abs(arr[i])>Math.abs(arr[j])){
+            System.out.println(arr[i] +" "+arr[j] + " Absolute Value "+ Math.abs(arr[i]) +" "+Math.abs(arr[j]));
+        results[p]= arr[i]*arr[i];
+        i++;
+        }else {
+            System.out.println(arr[i] +" "+arr[j] + " Absolute Value "+ Math.abs(arr[i]) +" "+Math.abs(arr[j]));
+
+            results[p]= arr[j]*arr[j];
+            j--;
+        }
+
+    }
+    return results;
+}
+    public static void main(String[] args) {
+    int[] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sqrOfSortedArrays(nums)));
+    }
 
 }
