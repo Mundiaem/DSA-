4d27a45552fcdf7e133ff7bb42a68e5876e1e409
Mundiaem <machariamundia@gmail.com>
2023-01-04 21:03:02 +0300

    String --

---
+++ src/Graph/AllPossiblePathToTarget.java
@@ -0,0 +1,32 @@
+package Graph;
+
+/**
+ * created with love by mundiaem
+ * created on 30/12/2022
+ * Time: 17:19
+ * ⚡  - DSA-
+ */
+
+/*
+*
+* Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1,
+* find all possible paths from node 0 to node n - 1 and return them in any order.
+
+The graph is given as follows:
+* graph[i] is a list of all nodes you can visit from node i
+*  (i.e., there is a directed edge from node i to node graph[i][j]).
+
+
+
+Example 1:
+
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+* */
+//https://www.simplilearn.com/tutorials/data-structure-tutorial/backtracking-algorithm
+    //https://www.geeksforgeeks.org/backtracking-algorithms/
+    
+public class AllPossiblePathToTarget {
+}
---
+++ src/string/Lexicographically.java
@@ -0,0 +1,21 @@
+package string;
+
+/**
+ * created with love by mundiaem
+ * created on 04/01/2023
+ * Time: 21:00
+ * ⚡  - DSA-
+ */
+
+public class Lexicographically {
+
+    /*
+    *https://leetcode.com/problems/permutation-in-string/?envType=study-plan&id=algorithm-i
+    * https://leetcode.com/problems/isomorphic-strings/description/
+    * https://leetcode.com/problems/word-pattern-ii/description/
+    * https://leetcode.com/problems/delete-columns-to-make-sorted/
+    * https://leetcode.com/problems/maximum-trailing-zeros-in-a-cornered-path/
+    * https://leetcode.com/problems/odd-string-difference/description/
+    * https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/solutions/2995198/java-o-n-hashmap/
+    * */
+}
