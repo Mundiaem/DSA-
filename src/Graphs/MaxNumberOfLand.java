7997ffe1fa677e639fd6d6d1278ea6b6634edc74
Mundiaem <machariamundia@gmail.com>
2023-01-07 15:58:46 +0300

    max number of island

---
+++ src/Graphs/MaxNumberOfLand.java
@@ -0,0 +1,16 @@
+package Graphs;
+
+/**
+ * created with love by mundiaem
+ * created on 07/01/2023
+ * Time: 15:54
+ * ⚡  - DSA-
+ */
+/*
+* https://www.tutorialcup.com/interview/graph/max-area-of-island.htm
+*https://dev.to/seanpgallivan/solution-max-area-of-island-4njk
+* https://medium.com/the-code-shelf/day-20-the-max-area-of-island-problem-29638dc344dd
+* https://algorithms.tutorialhorizon.com/number-of-islands/
+* */
+public class MaxNumberOfLand {
+}
