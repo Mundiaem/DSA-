676e236c8dc397374830355bcd0c5f04b815ed6b
Mundiaem <machariamundia@gmail.com>
2023-01-07 16:35:35 +0300

    merge two binary tree

---
+++ src/BST/MergeTwoBinaryTree.java
@@ -0,0 +1,16 @@
+package BST;
+
+/**
+ * created with love by mundiaem
+ * created on 07/01/2023
+ * Time: 16:13
+ * ⚡  - DSA-
+ */
+/*
+*https://www.topcoder.com/thrive/articles/merge-two-binary-trees
+* https://www.geeksforgeeks.org/merge-two-binary-trees-node-sum/
+* https://www.enjoyalgorithms.com/blog/merge-binary-tree
+* https://afteracademy.com/blog/merge-two-binary-trees/
+* */
+public class MergeTwoBinaryTree {
+}
